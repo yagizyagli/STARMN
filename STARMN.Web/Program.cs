855e677052f58a44a6eb9b92e8be8b699b2511a0
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-07-25 16:49:08 +0300

    feat: add repository and service registrations to Program.cs

--- STARMN.Web/Program.cs
+++ STARMN.Web/Program.cs
@@ -1,22 +1,44 @@
 using Microsoft.EntityFrameworkCore;
+using STARMN.Access.Repositories;
+using STARMN.Access.Repositories.Interfaces;
 using STARMN.Database;
+using STARMN.Service.Services;
+using STARMN.Service.Services.Interfaces;
 
 namespace STARMN.Web;
    
 public class Program
 {
         public static void Main(string[] args)
-     {
+        {
 
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
 
-
-
         var connectionAddress = builder.Configuration.GetConnectionString("STARMNDBConnection");
         builder.Services.AddSqlServer<STARMNDB>(connectionAddress);
+
+
+        // Add services and repositories to the DI container
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+        builder.Services.AddScoped<IBasketService, BasketService>();
+        builder.Services.AddScoped<IOrderService, OrderService>();
+        builder.Services.AddScoped<IOrderDetailService, OrderDetailService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<IRoleService,RoleService>();
+
+
+        builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+        builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+        builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+        builder.Services.AddHttpContextAccessor();
 
         var app = builder.Build();
 
